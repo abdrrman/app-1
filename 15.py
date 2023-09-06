830692ef881d1c674ef2d791e60f90c2983c5460
Ubuntu <ubuntu@ip-172-31-42-248.eu-central-1.compute.internal>
2023-09-06 21:15:40 +0000

    req

--- 15.py
+++ 15.py
@@ -2,4 +2,5 @@ import streamlit as st
 st.title("DemoGPT")
 st.header("Edit test")
 if st.button("Show"):
-    st.success("You've just pressed button!")
+    st.balloons()
+    st.success("Hurray!")
