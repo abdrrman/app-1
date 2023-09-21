7c00e47400ba9ff731088dcac6383645e136f9d9
abdrrman <ubuntu@ip-172-31-42-248.eu-central-1.compute.internal>
2023-09-21 22:44:04 +0000

    req

--- 53.py
+++ 53.py
@@ -26,7 +26,7 @@ openai_api_key = st.sidebar.text_input(
     type="password",
 )
 
-st.title('My Psychologist')
+st.title('My Psychologist v2')
 #Get message from the user for the chat-based system
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):  
