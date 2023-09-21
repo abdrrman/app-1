053452c1e6c052e958730c6a1d76c2b05fe8fd73
abdrrman <ubuntu@ip-172-31-42-248.eu-central-1.compute.internal>
2023-09-21 08:47:50 +0000

    req

--- 24.py
+++ 24.py
@@ -1,5 +1,5 @@
 
 import streamlit as st
 if st.button('Learn'):
-    st.success("I really really really love Nur ❤️
-She is my heart!")
+    st.success('I really really really love Nur ❤️
+She is my heart!')
